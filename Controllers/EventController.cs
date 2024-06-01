7230c958149c34f8e4bb9ad26cb2c7c8d16322ee
jGrzyb <jgrzyb@student.agh.edu.pl>
2024-06-01 20:43:18 +0200

    (temp, please Rider) adding tables

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,148 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using projekt.Data;
 using projekt.Models;
 
-namespace projekt.Controllers;
-
-public class EventController : Controller
+namespace projekt.Controllers
 {
-    private readonly DataBaseContext _context;
-
-    public EventController(DataBaseContext context)
+    public class EventController : Controller
     {
-        _context = context;
-    }
+        private readonly DataBaseContext _context;
 
-    [HttpGet, ActionName("Index")]
-    public async Task<IActionResult> Index()
-    {
-        return View(await _context.Events.ToListAsync());
-    }
-
-    [HttpGet, ActionName("Details")]
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null)
+        public EventController(DataBaseContext context)
         {
-            return NotFound();
+            _context = context;
         }
 
-        var @event = await _context.Events
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (@event == null)
+        // GET: Event
+        public async Task<IActionResult> Index()
         {
-            return NotFound();
+            return View(await _context.Event.ToListAsync());
         }
 
-        return View(@event);
-    }
-
-    [HttpGet, ActionName("Create")]
-    public IActionResult Create()
-    {
-        return View();
-    }
-
-    [HttpPost, ActionName("Create"), ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,UserId,Name,Description,Date")] Event @event)
-    {
-        if (!ModelState.IsValid)
+        // GET: Event/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            return View(@event);
-        }
-        
-        _context.Add(@event);
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-
-    }
-
-    [HttpGet, ActionName("Edit")]
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
-        }
-
-        var @event = await _context.Events.FindAsync(id);
-        if (@event == null)
-        {
-            return NotFound();
-        }
-
-        return View(@event);
-    }
-
-    [HttpPost, ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,Name,Description,Date")] Event @event)
-    {
-        if (id != @event.Id)
-        {
-            return NotFound();
-        }
-
-        if (!ModelState.IsValid)
-        {
-            return View(@event);
-        }
-        
-        try
-        {
-            _context.Update(@event);
-            await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            if (!EventExists(@event.Id))
+            if (id == null)
             {
                 return NotFound();
             }
-            else
+
+            var @event = await _context.Event
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@event == null)
             {
-                throw;
+                return NotFound();
             }
+
+            return View(@event);
         }
 
-        return RedirectToAction(nameof(Index));
-
-    }
-
-    [HttpGet, ActionName("Delete")]
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null)
+        // GET: Event/Create
+        public IActionResult Create()
         {
-            return NotFound();
+            return View();
         }
 
-        var @event = await _context.Events
-            .FirstOrDefaultAsync(m => m.Id == id);
-        
-        if (@event == null)
+        // POST: Event/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CalendarId,Name,Description,Date")] Event @event)
         {
-            return NotFound();
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
         }
 
-        return View(@event);
-    }
-
-    [HttpPost, ActionName("Delete"), ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        var @event = await _context.Events.FindAsync(id);
-        if (@event != null)
+        // GET: Event/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            _context.Events.Remove(@event);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event.FindAsync(id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+            return View(@event);
         }
 
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-    }
+        // POST: Event/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CalendarId,Name,Description,Date")] Event @event)
+        {
+            if (id != @event.Id)
+            {
+                return NotFound();
+            }
 
-    private bool EventExists(int id)
-    {
-        return (_context.Events?.Any(e => e.Id == id)).GetValueOrDefault();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(@event.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@event);
+        }
+
+        // GET: Event/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // POST: Event/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @event = await _context.Event.FindAsync(id);
+            if (@event != null)
+            {
+                _context.Event.Remove(@event);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Event.Any(e => e.Id == id);
+        }
     }
 }
--- Data/DataBaseContext.cs
+++ Data/DataBaseContext.cs
@@ -1,16 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using projekt.Models;
 
-namespace projekt.Data;
-
-public class DataBaseContext : DbContext
+namespace projekt.Data
 {
-    public DataBaseContext (DbContextOptions<DataBaseContext> options)
-        : base(options)
+    public class DataBaseContext : DbContext
     {
+        public DataBaseContext (DbContextOptions<DataBaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<projekt.Models.Calendar> Calendar { get; set; } = default!;
+        public DbSet<projekt.Models.Event> Event { get; set; } = default!;
+        public DbSet<projekt.Models.Ownership> Ownership { get; set; } = default!;
+        public DbSet<projekt.Models.TaskEvent> TaskEvent { get; set; } = default!;
     }
-
-    public DbSet<User> Users { get; set; } = default!;
-
-    public DbSet<Event> Events { get; set; } = default!;
 }
