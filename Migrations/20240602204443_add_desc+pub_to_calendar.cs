08a5f72726b0e00ff19b8ea03de53284c2a7f52c
jGrzyb <jgrzyb@student.agh.edu.pl>
2024-06-03 00:53:44 +0200

    migrations

---
+++ Migrations/20240602204443_add_desc+pub_to_calendar.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_descpub_to_calendar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Calendar",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublic",
+                table: "Calendar",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Calendar");
+
+            migrationBuilder.DropColumn(
+                name: "IsPublic",
+                table: "Calendar");
+        }
+    }
+}
