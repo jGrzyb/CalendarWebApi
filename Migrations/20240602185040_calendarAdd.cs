41527729427a099e9e70adcc43d1695cd3d60fc8
jGrzyb <jgrzyb@student.agh.edu.pl>
2024-06-02 21:14:40 +0200

    migrations

---
+++ Migrations/20240602185040_calendarAdd.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class calendarAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Calendar",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Calendar",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Calendar");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Calendar");
+        }
+    }
+}
